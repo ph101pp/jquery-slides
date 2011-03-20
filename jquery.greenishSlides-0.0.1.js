b1cba425526692c081554fef69530e6f69e586d7
Philipp C. Adrian <philippcadrian@greenish.ch>
2011-03-20 02:50:22 -0400

    tiding up code and starting to implement vertical orientation.

--- jquery.greenishSlides-0.0.1.js
+++ jquery.greenishSlides-0.0.1.js
@@ -49,9 +49,21 @@ $.extend($.gS, {
 		$.gS.settings = $.extend(this.defaults, typeof(options) == "object" ? options :{});
 
 //		Sets wrappers and additional classes
-		var slides = $(context).css({overflow:"hidden"}).children().addClass("gSSlide").css($.gS.css.gSSlide);
-		$.gS.settings.orientation == "horizontal" ? slides.addClass("gSHorizontal").css($.gS.css.gSHorizontal) : slides.addClass("gSVertical").css($.gS.css.gSVertical);
-
+		var slides = $(context).css($.gS.css.context).children().addClass("gSSlide").css($.gS.css.gSSlide);
+		if($.gS.settings.orientation == "horizontal") {
+			slides.addClass("gSHorizontal").css($.gS.css.gSHorizontal);
+			$.gS.ToL="top";
+			$.gS.WoH="width";
+			$.gS.LoT="left";
+			$.gS.RoB="right";
+		}
+		else {
+			slides.addClass("gSVertical").css($.gS.css.gSVertical);
+			$.gS.ToL="left";
+			$.gS.WoH="height";
+			$.gS.LoT="top";
+			$.gS.RoB="bottom";
+		}
 		$.gS.initSlides(slides);
 
 //		First Initialisation		
@@ -111,82 +123,80 @@ $.extend($.gS, {
 ////////////////////////////////////////////////////////////////////////////////
 	getValues : function (context) {
 		var slides=$(context).children();
-		var t={ai:slides.filter(".gSSlide.active").index(), minus:0, c:0, cW:$(context).width(), cH:$(context).height(), slides:{}};
-		var css = [];
+		var t={css:{}, slides:{}, ai:slides.filter(".gSSlide.active").index(), cS:$(context)["width"]()};
 //		get minWidth for every slide.
-		for(var i=0; i < slides.length; i++) if(i != t.ai) {
-				t.slides[i]=slides.eq(i);
-				css[i] = { "width" : parseFloat(t.slides[i].css("min-width").replace("px",""))};
-				t.c+=css[i]["width"];
-			}
-			else {
-				t.slides[i]=slides.eq(i);
-				css[i]={};
-				
+		for(var i=t.c=0; i < slides.length; i++) {
+			t.css[i] = {};
+			t.slides[i]=slides.eq(i);
+			if(i == t.ai) {
 //				If there is an max-width defined for the active element - set it to the new width.
-				t.max = parseFloat(t.slides[t.ai].css("max-width").replace("px",""));
-				if(t.max > 0) css[t.ai]["width"]=t.max;
+				t.max = parseFloat(t.slides[t.ai].css("max-"+$.gS.WoH).replace("px",""));
+				if(t.max > 0) t.css[t.ai][$.gS.WoH]=t.max;
 			}
-
+			else t.c+=t.css[i][$.gS.WoH]=parseFloat(t.slides[i].css("min-"+$.gS.WoH).replace("px",""));
+		}
 //		If fillSpace is Set (kwicks)
-		if($.gS.settings.fillSpace) {
+		if(true || $.gS.settings.fillSpace)
 //			if no max-width is set for the active element, it's filling all the space it can get. (everything else stays on min-width)
-			if(true && t.ai >= 0 && (!(t.max>0) || t.max>t["cW"]-t.c)) {
-				css[t.ai]["width"] = t["cW"]-t.c;
+			if(true && t.ai >= 0 && (!(t.max>0) || t.max>t.cS-t.c)) {
+				t.css[t.ai][$.gS.WoH] = t.cS-t.c;
 			}
 			else {
 //				Calculates which size elements have, that are not hitting any max/min limit.
-				var fullSize=t["cW"];
+				var fullSize=t.cS;
 				var count=slides.length
 				var newSize=Math.ceil(fullSize/count);
 				var skip=[];
 				
 				for(var i=0; i < slides.length; i++) 
-					if(!skip[i] && (css[i]["width"] > newSize || (true && i == t.ai))) {
+					if(!skip[i] && (t.css[i][$.gS.WoH] > newSize || (true && i == t.ai))) {
 						skip[i]=true;
 						count--;
-						fullSize-=css[i]["width"];
+						fullSize-=t.css[i][$.gS.WoH];
 						newSize=Math.ceil(fullSize/count);
 						i=-1;
 					}
 //				Sets calculated value.
-				for(var i=0; i < slides.length; i++) if(!skip[i]) css[i]["width"]=newSize;
+				for(var i=0; i < slides.length; i++) if(!skip[i]) t.css[i][$.gS.WoH]=newSize;
 			}
-		}
-		for(var i=0; i < slides.length; i++) {
-			t.minus+=css[i]["width"];
+		for(var i=t.c=0; i < slides.length; i++) {
+			t.c+=t.css[i][$.gS.WoH];
 			if(true && i==t.ai) {
-				if(t.minus-t.slides[i].css("width")<t["cW"]-t.minus) $.gS.positioning(context,  t.slides[i], "left", true);
-				else  $.gS.positioning(context,  t.slides[i], "right", true);
+				if(t.c-t.slides[i].css($.gS.WoH)<t.cS-t.c) $.gS.positioning(context,  t.slides[i], $.gS.LoT, true);
+				else  $.gS.positioning(context,  t.slides[i], $.gS.RoB, true);
 				
-				css[i]["margin-left"]=t.minus-css[i]["width"];
-				css[i]["margin-right"]=t["cW"]-t.minus;
-				css[i]["width"]="auto";
+				t.css[i]["margin-"+$.gS.LoT]=t.c-t.css[i][$.gS.WoH];
+				t.css[i]["margin-"+$.gS.RoB]=t.cS-t.c;
+				t.css[i][$.gS.WoH]="auto";
 			}
-			else if(t.minus-css[i]["width"]<t["cW"]-t.minus || t.ai<0){
-				$.gS.positioning(context,  t.slides[i], "left");
-				css[i]["left"]=t.minus-css[i]["width"];
+			else if(t.c-t.css[i][$.gS.WoH]<t.cS-t.c || t.ai<0){
+				$.gS.positioning(context,  t.slides[i], $.gS.LoT);
+				t.css[i][$.gS.LoT]=t.c-t.css[i][$.gS.WoH];
 			}
 			else {
-				$.gS.positioning(context, t.slides[i], "right");
-				css[i]["right"]=t["cW"]-t.minus;
+				$.gS.positioning(context, t.slides[i], $.gS.RoB);
+				t.css[i][$.gS.RoB]=t.cS-t.c;
 			}
 		}
-			
-		return css;
+		return t.css;
 	},
 ////////////////////////////////////////////////////////////////////////////////
 	positioning : function (context, obj, bind, active) {
-		var t={p : obj.offset(), "bind":bind, "from": bind=="left"?"right":"left", cW:context.innerWidth(), cH:context.innerHeight(), oW:obj.outerWidth(), oH:obj.outerHeight()};
+		var t={p : obj.offset(), "bind":bind, "from": bind==$.gS.LoT?$.gS.RoB:$.gS.LoT, cS:$(context)["inner"+$.gS.WoH.charAt(0).toUpperCase() + $.gS.WoH.slice(1)](), oS:obj["outer"+$.gS.WoH.charAt(0).toUpperCase() + $.gS.WoH.slice(1)]()};
 		
 		if(active) {
-			t.css={zIndex:0, width: "auto", "margin-left":t.p.left, "margin-right":t["cW"]-t.p.left-t["oW"], position:"static"};
+			t.css={zIndex:0, position:"static"};
+			t.css[$.gS.WoH]="auto";
+			t.css["margin-"+$.gS.LoT]=t.p.left;
+			t.css["margin-"+$.gS.RoB]=t.cS-t.p.left-t.oS;
 			t.css[bind]=0;
 		}
 		else {
-			t.css={zIndex:1, position:"absolute", top:0, "margin-left":0, "margin-right":0, "width":obj.outerWidth()};
+			t.css={zIndex:1, position:"absolute"};
 			if(parseFloat(obj.css("margin-"+t.bind).replace("px","")) > 0 || parseFloat(obj.css("margin-"+t.from).replace("px","")) > 0) t.css[t.bind]=parseFloat(obj.css("margin-"+t.bind).replace("px",""));
-			else t.bind=="left" ? t.css[bind]=t.p.left : t.css[bind]=t["cW"]-t.p.left-t["oW"];
+			else t.bind==$.gS.LoT ? t.css[bind]=t.p.left : t.css[bind]=t.cS-t.p.left-t.oS;
+			t.css[$.gS.ToL]=t.css["margin-"+$.gS.LoT]=t.css["margin-"+$.gS.RoB]=0;
+			t.css[$.gS.WoH]=t.oS;
 		}
 		t.css[t.from]="auto";
 		obj.stop().css(t.css);
@@ -216,6 +226,9 @@ $.extend($.gS, {
 	},	
 ////////////////////////////////////////////////////////////////////////////////
 	css :{
+		context : {
+			overflow:"hidden"
+		},
 		gSSlide : {
 			position:"absolute",
 			margin:0,
