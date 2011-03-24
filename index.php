7a5514bda514abd9f00c740d1ca906191a44996f
Philipp C. Adrian <philippcadrian@greenish.ch>
2011-03-23 21:46:06 -0400

    Art of the Book behavior works with the preActivate Hook! SUCCESS!

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
 <!--		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
 		<script type="text/javascript" src="http://jmar777.googlecode.com/svn/trunk/js/jquery.easing.1.3.js"></script>-->
 		<script type="text/javascript" src="jquery.greenishSlides-0.0.1.js"></script>
-		<script type="text/javascript" src="jquery.jswipe-0.1.2.j"></script>
+		<script type="text/javascript" src="jquery-ui-1.8.11.custom.min.js"></script>
+		<script type="text/javascript" src="jquery.jswipe-0.1.2.js"></script>
 		<script type="text/javascript">
 			(function($) {
 				$(document).ready(function() { 
@@ -19,13 +20,18 @@
 				 		activateEvent: "click",
 						hooks: {
  							preActivate: function (slide, css) {
+ 								var colors=["#EEEEEE","#DDDDDD","#CCCCCC","#BBBBBB","#AAAAAA","#999999","#888888","#777777","#666666","#555555","#444444","#333333","#222222","#111111"];
 								var context=slide.parent();
-								var ai=context.find(".active").index();
+									context.children().stop().animate({"width":"0px"},"slow").css({"min-width":"0px", backgroundColor:"#000000"});
+								var ai=slide.css({"backgroundColor":"#ffffff"}).index();
 								var width=40;
-								for(var i=1; i<=6; i++) {
-									width/=2;
-									if(ai-i >=0) context.children().eq(ai-i).css({"min-width":width});
-									context.children().eq(ai+i).css({"min-width":width});
+									
+
+								for(var i=1; i<=20; i++) {
+									width/=1.2;
+									if(ai-i >=0) context.children().eq(ai-i).stop().animate({"width":width},"slow").css({"min-width":width, backgroundColor:colors[i]});
+									
+									if(ai+i <= context.children().length) context.children().eq(ai+i).stop().animate({"width":width},"slow").css({"min-width":width, backgroundColor:colors[i]});
 								}
  								
  								
@@ -46,6 +52,21 @@
 	<body>
 		<article class="greenishSlides">
 			<section class="one"><img src="http://placehold.it/500x300"></section>
+			<section class="two"><img src="http://placekitten.com/200/300"></section>
+			<section class="three active"><img src="http://placehold.it/500x300"></section>
+			<section class="four"><img src="http://placekitten.com/200/300"></section>
+			<section class="two"><img src="http://placekitten.com/200/300"></section>
+			<section class="three active"><img src="http://placehold.it/500x300"></section>
+			<section class="four"><img src="http://placekitten.com/200/300"></section>
+			<section class="two"><img src="http://placekitten.com/200/300"></section>
+			<section class="three active"><img src="http://placehold.it/500x300"></section>
+			<section class="four"><img src="http://placekitten.com/200/300"></section>
+			<section class="two"><img src="http://placekitten.com/200/300"></section>
+			<section class="three active"><img src="http://placehold.it/500x300"></section>
+			<section class="four"><img src="http://placekitten.com/200/300"></section>
+			<section class="two"><img src="http://placekitten.com/200/300"></section>
+			<section class="three active"><img src="http://placehold.it/500x300"></section>
+			<section class="four"><img src="http://placekitten.com/200/300"></section>
 			<section class="two"><img src="http://placekitten.com/200/300"></section>
 			<section class="three active"><img src="http://placehold.it/500x300"></section>
 			<section class="four"><img src="http://placekitten.com/200/300"></section>
