eee9193100b436f450e318e8e04a942e28bcc0e0
Philipp C. Adrian <philippcadrian@greenish.ch>
2012-03-18 15:42:08 +0100

    Adding Examples and updating greenishSlides

--- greenishPlugins.js
+++ greenishPlugins.js
@@ -1,21 +1,101 @@
 (function($){
+////////////////////////////////////////////////////////////////////////////////
+	var event = function (event, ui, options){
+		switch($(ui.newHeader).attr("id")) {
+			case "example1" : 
+			default: 
+			break;
+			case "example2":
+				options={
+					vertical:true,
+					events: {
+						activate:"click",
+						deactivate:"click"
+					}
+				};
+			break;
+			case "example3":
+				options={
+					limits: {
+						min:30,
+					}
+				};
+			break;
+			case "example4":
+				options={
+					limits: {
+						min:10,
+						0: {
+							min:180
+						},
+						2: {
+							min:180
+						},
+						4: {
+							min:180
+						},
+						"-1": {
+							min:180
+						}
+					}
+				};
+			break;
+			case "example5":
+				options={
+					active:0,	
+					stayOpen:true,
+					keyEvents:true,
+					limits: {
+						max:400,
+					}
+				};
+			break;
+			case "example6":
+				options={	
+					stayOpen:true,
+					events: {
+						activate:"click",
+						deactivate:"click"
+					},
+					limits: {
+						0: {
+							min:30,
+							max:30
+						}
+					}
+				};
+			break;
+		}
+		$(".myElement").greenishSlides($.extend(true, {}, $.gS.defaults, {
+			limits : {
+				min:false,
+				max:false,
+				0:false,
+				1:false,
+				2:false,
+				3:false,
+				4:false,
+				5:false,
+				6:false,
+				"-1":false,
+				"-2":false,
+				"-3":false,
+				"-4":false,
+				"-5":false,
+				"-6":false,
+				"-7":false
+			}
+		}, options||{}));
+
+	}
+////////////////////////////////////////////////////////////////////////////////
 	$(document).ready(function() { 
-		$(".myElement").greenishSlides({
-			events: {
-				activate:"mouseover",
-				deactivate:"mouseout"
-			},
-			limits: {
-				min:30
-			}
-		});	
-		
 		$(".examples").accordion({
-			active:1,
 			header:".handle",
-			change: function (event, ui){
-				console.log("hange")
-			}
+			autoHeight:false,
+			create: event,
+			change: event
 		});
 	});
+////////////////////////////////////////////////////////////////////////////////
 })(jQuery);
